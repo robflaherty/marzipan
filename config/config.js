4363ff14347bfb20d20ce8c565bb2b7e32b8ad9e
Rob Flaherty <rob@ravelrumba.com>
2012-04-27 12:35:50 -0400

    removing all old files

--- config/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = config = {
-  "ping_port" : 8000,
-  "dashboard_port" : 8080,
-  "dashboard" : true
-};
--- readme.md
+++
@@ -1,1 +0,0 @@
-Front end for simple in-house user tracking. It's only half-baked; don't use in production!
